4854cc5ccc2050fd6d296ce20b95eae07755f785
Rafademetrio <rafademetrio@gmail.com>
2024-02-02 21:48:15 -0300

    classe Ponte anotada com lombok.

--- rios_pontes_and_overdrives/src/main/java/com/rafademetrio/riospontesandoverdrives/controllers/PonteController.java
+++ rios_pontes_and_overdrives/src/main/java/com/rafademetrio/riospontesandoverdrives/controllers/PonteController.java
@@ -6,10 +6,7 @@ import com.rafademetrio.riospontesandoverdrives.models.PonteOutputDTO;
 import com.rafademetrio.riospontesandoverdrives.services.PonteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/ponte")
@@ -22,6 +19,7 @@ public class PonteController {
     public ResponseEntity<PonteOutputDTO> criarNovoRelatorio(@RequestBody PonteInputDTO ponteInputDTO){
         return this.ponteService.save(ponteInputDTO);
     }
+
 
 
 
--- rios_pontes_and_overdrives/src/main/java/com/rafademetrio/riospontesandoverdrives/models/Ponte.java
+++ rios_pontes_and_overdrives/src/main/java/com/rafademetrio/riospontesandoverdrives/models/Ponte.java
@@ -1,29 +1,20 @@
 package com.rafademetrio.riospontesandoverdrives.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
 import java.io.IOException;
 import java.math.BigInteger;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Ponte {
-
-    public Ponte() {
-    }
-
-    public Ponte(BigInteger id, String km, String linha, String cidade, String estado, String naturezaDaTransposicao, String bitola, String tracado, String trilhos, String fixacao, String comprimento, String largura) {
-        this.id = id;
-        this.km = km;
-        this.linha = linha;
-        this.cidade = cidade;
-        this.estado = estado;
-        this.naturezaDaTransposicao = naturezaDaTransposicao;
-        this.bitola = bitola;
-        this.tracado = tracado;
-        this.trilhos = trilhos;
-        this.fixacao = fixacao;
-        this.comprimento = comprimento;
-        this.largura = largura;
-    }
-
     public Ponte(PonteInputDTO ponteInputDTO){
         this.id = ponteInputDTO.getId();
         this.km = ponteInputDTO.getKm();
@@ -56,101 +47,7 @@ public class Ponte {
     private String comprimento;
     private String largura;
 
-    public BigInteger getId() {
-        return id;
-    }
 
-    public void setId(BigInteger id) {
-        this.id = id;
-    }
-
-    public String getKm() {
-        return km;
-    }
-
-    public void setKm(String km) {
-        this.km = km;
-    }
-
-    public String getLinha() {
-        return linha;
-    }
-
-    public void setLinha(String linha) {
-        this.linha = linha;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getNaturezaDaTransposicao() {
-        return naturezaDaTransposicao;
-    }
-
-    public void setNaturezaDaTransposicao(String naturezaDaTransposicao) {
-        this.naturezaDaTransposicao = naturezaDaTransposicao;
-    }
-
-    public String getBitola() {
-        return bitola;
-    }
-
-    public void setBitola(String bitola) {
-        this.bitola = bitola;
-    }
-
-    public String getTracado() {
-        return tracado;
-    }
-
-    public void setTracado(String tracado) {
-        this.tracado = tracado;
-    }
-
-    public String getTrilhos() {
-        return trilhos;
-    }
-
-    public void setTrilhos(String trilhos) {
-        this.trilhos = trilhos;
-    }
-
-    public String getFixacao() {
-        return fixacao;
-    }
-
-    public void setFixacao(String fixacao) {
-        this.fixacao = fixacao;
-    }
-
-    public String getComprimento() {
-        return comprimento;
-    }
-
-    public void setComprimento(String comprimento) {
-        this.comprimento = comprimento;
-    }
-
-    public String getLargura() {
-        return largura;
-    }
-
-    public void setLargura(String largura) {
-        this.largura = largura;
-    }
 
     public static void gerarRelatorio(Ponte ponte) throws IOException {
         Relatorio.gerarRelatorio(ponte);
